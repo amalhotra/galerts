5d1b221a1fc678b3ee94e5cbbfb3099bd281a42b
Ankit <ankit@fast.(none)>
2011-12-27 13:19:22 -0500

    added verify and refactored defaults

--- lib/galerts.rb
+++ lib/galerts.rb
@@ -1,7 +1,54 @@
 require "galerts/version"
 
 module Galerts
-  # Your code goes here...
+	MAX_Q_LEN = 2048 # Maximum len of query
+
+	EMAIL_DELIVERY = 'Email'
+	FEED_DELIVERY = 'feed'
+	DEFAULT_DELIVERY = '6'
+
+	DELIVERY_TYPES = {
+		EMAIL_DELIVERY => '0',
+		FEED_DELIVERY => DEFAULT_DELIVERY
+	}
+
+	RT = 'As-it-happens'
+	DAILY = 'Once a day'
+	WEEKLY = 'Once a week'
+
+	FREQS_TYPES = {
+		RT => '0',
+		DAILY => '1',
+		WEEKLY => '6'
+	}
+
+	BEST_VOL = 'Only the best results'
+	ALL_VOL = 'All results'
+
+	VOLS_TYPES = {
+		BEST_VOL => '0',
+		ALL_VOL => '1'
+	}
+
+	EVERYTHING = 'Everything'
+	NEWS = 'News' 
+	BLOGS = 'Blogs'
+	REALTIME = 'Realtime'
+	VIDEO = 'Video'
+	DISCUSSIONS = 'Discussions'
+
+	ALERT_TYPES = {
+		EVERYTHING => '7',
+		NEWS => '1',
+		BLOGS => '4',
+		REALTIME => '20',
+		VIDEO => '9',
+		DISCUSSIONS => '9'
+	}
+
+	ACTIVE_ALERT_CLASS = "ACTIVE"
+	UNVERIFIED_ALERT_CLASS = "UNVERIFIED"
+
 end
 
 require 'galerts/google_defaults'
--- lib/galerts/alerts_manager.rb
+++ lib/galerts/alerts_manager.rb
@@ -3,24 +3,28 @@ require 'mechanize'
 module Galerts
 	class AlertsManager
 	
-		include Galerts::GoogleDefaults
+		#include Galerts::GoogleDefaults
 
-		attr_reader :auth_domains,:email
+		# Allowed to read auth_domains and email
+		attr_reader :auth_domains,:email,:agent
 
 		def initialize(email,password)
 			email+='@gmail.com' if email.index('@').nil?
 			@email = email
 			domain = "com"
+			# Unfortunately we need to store this since we have to login to multiple google domains
 			@password = password
+			@auth_domains = []
 			init_agent
 			login(domain)
-			@auth_domains = ["com"]
 		end
 
 		def to_s
 			"<#{self.class} email=#{@email} auth_domains=#{@auth_domains}>"
 		end
 
+		# Get all alerts as an array of Alert Objects
+		# Note: We always get alerts from google.com/alerts/manage as all our mappings in GoogleDefaults are in English
 		def alerts
 			g_alerts = []
 			alerts_page = @agent.get(alerts_url("/manage"))
@@ -28,10 +32,17 @@ module Galerts
 			type = EVERYTHING
 			alert_rows.each do |alert_row|
 				tds = alert_row.css('td')
+
+				# If this row has no columns, it must be a row for type definition
 				if tds.empty?
 					type = alert_row.css('th')[1].text
 					next
 				end
+
+				# Here only if columns exist and if the length of the columns < 6, it means there are no alerts
+				next if tds.size < 6
+
+				# Alerts do exist: Build alert object
 				s = tds[0].css('input[name=s]').first["value"]
 				query = tds[1].text
 				search_query = tds[1].css('a').first["href"]
@@ -45,14 +56,17 @@ module Galerts
 					delivery = FEED_DELIVERY
 				end
 				email = @email # TODO: Could be one of many email addresses associated with account
-				g_alerts << Alert.new(email,query,search_query,type,frequency,volume,delivery,s,feed_url)
+				state = alert_row.attributes["class"].value
+				g_alerts << Alert.new(email,query,search_query,type,frequency,volume,delivery,s,state,feed_url)
 			end	
 			g_alerts
 		end
 
+		# Create a new alert
 		def create(query,domain = "com",type = EVERYTHING,frequency = RT,volume = ALL_VOL,feed=false)
+			# If we havent been authenticated for a domain, authenticate now
 			authenticate!(domain)
-			create_page = @agent.get(alerts_url("/create",domain))
+			create_page = @agent.get(alerts_url("",domain))
 			create_form = create_page.forms.first
 			create_form.q = query
 			create_form.e = feed ? FEED_DELIVERY : @email
@@ -60,11 +74,20 @@ module Galerts
 			create_form.t = ALERT_TYPES[type]
 			create_form.l = VOLS_TYPES[volume]
 			resp = @agent.submit(create_form)
-			# TODO: Return alert as GAlerts::Alert object just created
+			alert = find_by_query(query)
+			alert.nil? || alert.empty? || alert.active? ? alert : verify!(alert)
+			# TODO: Check for duplicate alert and return message
 		end
 
+		# Note: Google allows creating the same alert (same search query) if delivery is different
+		# (email and rss feed) and/or domains are different
+
+		# Update given alert
+		# TODO: Check duplicate as above
 		def update(alert)
 			authenticate!(alert.domain)
+
+			# Needs these to prevent xss
 			x,es,hps = scrape_x_es_hps(alert)
 			params = {
 				'd' => DELIVERY_TYPES[alert.delivery] || DEFAULT_DELIVERY,
@@ -79,8 +102,10 @@ module Galerts
 			}	
 			params['f'] = FREQS_TYPES[alert.frequency] if alert.delivery == EMAIL_DELIVERY
 			resp = @agent.post(alerts_url("/save",alert.domain),params)
+			true
 		end
 
+		# Delete an alert
 		def delete(alert)
 			params = {
 				'da' => 'Delete',
@@ -89,30 +114,48 @@ module Galerts
 				'x' => scrape_galx
 			}
 			resp = @agent.post(alerts_url("/save"),params)
+			true
+		end
+		
+		# Verify an alert (Mainly a workaround till multiple domains issue can be solved)
+		def verify!(alert)
+			resp = @agent.get(alerts_url("/verify"),{'s' => alert.s}) unless alert.active?
+			alert
 		end
 
+		# Return all alerts that match a given query
 		def find_by_query(query)
 			self.alerts.select{|a| a.query == query}
 		end
 
+		def inspect
+			to_s
+		end
+
 		private
 
+		# If domain is in auth_domains, we have already authenticated with that google domain
 		def authenticated?(domain)
 			!@auth_domains.index(domain).nil?
 		end
 
+		# Authenticate unless we already have
 		def authenticate!(domain)
 			return if authenticated?(domain)
 			login(domain)
 		end
 
+		# TODO: Better exception handling and messages
 		def login(domain="com")
 			login_page = @agent.get(login_url(domain))			
 			login_form = login_page.forms.first
 			login_form.Email = @email
 			login_form.Passwd = @password
 			login_resp = @agent.submit(login_form)
+			@auth_domains << domain
 		end
+
+		# Helpers to scrape xss prevention params
 
 		def scrape_galx
 			resp = @agent.get(alerts_url("/manage"))
@@ -127,14 +170,16 @@ module Galerts
 			[x,es,hps]
 		end
 
+		# Urls for Google Alerts
 		def login_url(domain="com")
-			"https://accounts.google.#{domain}/ServiceLogin?service=alerts&continue=#{alerts_url("",domain)}"
+			"https://accounts.google.com/ServiceLogin?service=alerts&continue=#{alerts_url("",domain)}"
 		end
 
 		def alerts_url(path = "",domain="com")
 			"http://www.google.#{domain}/alerts#{path}"
 		end
 
+		# Initialize Mechanize user agent
 		def init_agent
 			@agent = Mechanize.new
 			@agent.user_agent_alias = 'Linux Mozilla'
